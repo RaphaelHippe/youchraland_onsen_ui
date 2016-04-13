0f7c4fa8a0ebc298c20b84d8a9b6d8e7dfc2c7fe
youchra <nemmo@gmx.de>
2016-04-13 15:05:41 +0200

    new visual for left or right app, beer app started

--- www/js/app.js
+++ www/js/app.js
@@ -4,27 +4,50 @@ module.controller('AppController', function($scope) {});
 module.controller('PageController', function($scope) {
   ons.ready(function() {
     // Init code here
-    $scope.doSomething = function () {
-      console.log('test');
-    };
+
   });
 });
-module.controller('LeftOrRightCtrl', function($scope) {
+module.controller('BeerCtrl', function($scope) {
   ons.ready(function() {
     // Init code here
-    $scope.state = {
-      state: 0
-    };
-    $scope.tapIt = function () {
-      if (Math.floor((Math.random() * 10) + 1) % 2 === 0) {
-        $scope.state = {
-          state: 1
-        };
-      } else {
-        $scope.state = {
-          state: 2
-        };
-      }
-    };
+
   });
 });
+module.controller('LeftOrRightCtrl', ['$scope', '$timeout',
+  function($scope, $timeout) {
+    ons.ready(function() {
+      // Init code here
+      $scope.state = {
+        state: 0,
+        clicked: {
+          color: '#3498db'
+        }
+      };
+      $scope.doubleTapIt = function () {
+        console.log('test');
+        $timeout(function() {
+          $scope.state.clicked = {
+            color: '#f1c40f'
+            };
+        }, 500);
+        $scope.state.clicked = {
+          };
+      };
+      $scope.tapIt = function() {
+        $scope.state.clicked = {
+            color: '#c0392b'
+          };
+        $timeout(function() {
+          $scope.state.clicked = {
+              color: '#3498db'
+            };
+        }, 250);
+        if (Math.floor((Math.random() * 10) + 1) % 2 === 0) {
+          $scope.state.state = 1;
+        } else {
+          $scope.state.state = 2;
+        }
+      };
+    });
+  }
+]);
